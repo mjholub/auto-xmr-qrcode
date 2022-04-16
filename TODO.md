d8df57169c709a045a31f50e1b6101d5f6c75aa5
Marcelina Hołub <ovehis@riseup.net>
2022-04-16 05:20:27 +0200

    initial commit

---
+++ TODO.md
@@ -0,0 +1,4 @@
+1. Correctly loop the script
+2. Make the script daemonize
+3. Package the script
+4. Optimize stuff
---
+++ auto-xmr-qrcode.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+while true; do
+clipboard="$(xclip -o)"
+length=${#clipboard}
+dircheck() {
+	if [[ ! -d "$HOME/XMR Adress book" ]]; then
+	mkdir -p "$HOME/XMR Address book" && cd "$HOME/XMR Address book"
+	return
+	else
+	cd "$HOME/XMR Address book"
+	fi
+}
+
+if [ "$length" -eq 95 ]; then
+	dircheck
+	break
+	echo 'Press n for a temporary QR code (deleted after 10 minutes). To not generate a QR code, press q. Otherwise, type the filename:'
+	read -r filename
+	        if [ "$filename" = n ]; then
+			qrencode -o tmp.png "$clipboard"
+			xdg-open "tmp.png"
+			xclip /dev/null
+			sleep 600
+			rm tmp.png
+		elif [ "$filename" = q ]; then
+			echo "$clipboard " > tmpclip 
+			xclip tmpclip
+			rm tmpclip
+			clear
+		else
+			qrencode -o "$filename" "$clipboard"
+			xdg-open "$filename"
+			xclip /dev/null
+		fi
+	else 
+	sleep 60
+	clear
+	continue
+	fi
+sleep 15
+done
